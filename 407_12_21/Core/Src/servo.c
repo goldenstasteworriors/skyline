e77105d0403a0c126d2bba7fc2d1a7de4d1d38fd
Zexin Liang <138102353+ZexinLiang@users.noreply.github.com>
2023-12-24 22:48:15 +0800

    Add files via upload

---
+++ 407_12_21/Core/Src/servo.c
@@ -0,0 +1,25 @@
+#include "servo.h"
+#include "tim.h"
+
+void servoInit() {
+	HAL_TIM_PWM_Start(&htim10, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim11, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_3);
+	HAL_TIM_PWM_Start(&htim13, TIM_CHANNEL_1);
+}
+void servoSetAngle1(int angle) {
+	angle *= SERVODEGREEUNIT1;
+	__HAL_TIM_SET_COMPARE(&tim10, TIM_CHANNEL_1; angle);
+}
+void servoSetAngle2(int angle) {
+	angle *= SERVODEGREEUNIT2;
+	__HAL_TIM_SET_COMPARE(&tim11, TIM_CHANNEL_1; angle);
+}
+void servoSetAngle3(int angle) {
+	angle *= SERVODEGREEUNIT3;
+	__HAL_TIM_SET_COMPARE(&tim8, TIM_CHANNEL_3; angle);
+}
+void servoSetAngle4(int angle) {
+	angle *= SERVODEGREEUNIT4;
+	__HAL_TIM_SET_COMPARE(&tim13, TIM_CHANNEL_1; angle);
+}
