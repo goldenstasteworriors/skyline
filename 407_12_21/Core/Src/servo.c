88610a4883fafcacc1b5c461f291dcb6d95422f0
Zexin Liang <138102353+ZexinLiang@users.noreply.github.com>
2024-01-13 15:51:25 +0800

    Update servo.c

--- 407_12_21/Core/Src/servo.c
+++ 407_12_21/Core/Src/servo.c
@@ -9,17 +9,17 @@ void servoInit() {
 }
 void servoSetAngle1(int angle) {
 	angle *= SERVODEGREEUNIT1;
-	__HAL_TIM_SET_COMPARE(&tim10, TIM_CHANNEL_1; angle);
+	__HAL_TIM_SET_COMPARE(&tim10, TIM_CHANNEL_1, angle);
 }
 void servoSetAngle2(int angle) {
 	angle *= SERVODEGREEUNIT2;
-	__HAL_TIM_SET_COMPARE(&tim11, TIM_CHANNEL_1; angle);
+	__HAL_TIM_SET_COMPARE(&tim11, TIM_CHANNEL_1, angle);
 }
 void servoSetAngle3(int angle) {
 	angle *= SERVODEGREEUNIT3;
-	__HAL_TIM_SET_COMPARE(&tim8, TIM_CHANNEL_3; angle);
+	__HAL_TIM_SET_COMPARE(&tim8, TIM_CHANNEL_3, angle);
 }
 void servoSetAngle4(int angle) {
 	angle *= SERVODEGREEUNIT4;
-	__HAL_TIM_SET_COMPARE(&tim13, TIM_CHANNEL_1; angle);
+	__HAL_TIM_SET_COMPARE(&tim13, TIM_CHANNEL_1, angle);
 }
